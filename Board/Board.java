c1652ab34fcc0843a1e2bfa8384441274e037a54
krishan <krishan.jurel@gmail.com>
2020-09-07 11:34:17 +0800

    final code with 91% score

--- Board/Board.java
+++ Board/Board.java
@@ -4,56 +4,47 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Vector;
 
+
 public class Board {
 
     /* local copy of tiles */
     private int[][] tiles;
-    private int size;
+    private int row, col;
 
-    private Vector<Board> neighborBoards;
 
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
-        size = tiles.length;
-        this.tiles = new int[tiles.length][tiles.length];
-        for (int row = 0; row < tiles.length; row++) {
-            for (int col = 0; col < tiles.length; col++)
-                this.tiles[row][col] = tiles[row][col];
+        if (tiles == null) throw new java.lang.IllegalArgumentException();
+
+        row = tiles.length;
+        col = tiles[0].length;
+        this.tiles = new int[row][col];
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++)
+                this.tiles[r][c] = tiles[r][c];
         }
-        neighborBoards = new Vector<Board>();
     }
 
     // string representation of this board
     public String toString() {
-        String string = Integer.toString(size) + "\n";
-        for (int row = 0; row < size; row++) {
-            for (int col = 0; col < size; col++) {
-                string = " " + string.concat(Integer.toString(tiles[row][col])) + " ";
+        String string = Integer.toString(row) + "\n";
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                string = " " + string.concat(Integer.toString(tiles[r][c])) + " ";
             }
             string = string.concat("\n");
         }
         return string;
     }
 
-    private Board(int size) {
-        int row = 0, col = 0;
-        tiles = new int[size][size];
-        for (row = 0; row < size; row++) {
-            for (col = 0; col < size; col++) {
-                tiles[row][col] = (row * size + col);
-            }
-        }
-        /*set the last column to be zero */
-        tiles[row - 1][col - 1] = 0;
-    }
 
     private int BlankTile() {
         boolean _break = false;
-        int row = 0, col = 0;
-        for (row = 0; row < size; row++) {
-            for (col = 0; col < size; col++) {
-                if (tiles[row][col] == 0) {
+        int r = 0, c = 0;
+        for (r = 0; r < row; r++) {
+            for (c = 0; c < col; c++) {
+                if (tiles[r][c] == 0) {
                     _break = true;
                     break;
                 }
@@ -62,64 +53,13 @@ public class Board {
                 break;
 
         }
-        return row * size + col;
+        return r * row + c;
     }
 
-    private Vector<Integer> Neighbors() {
-        boolean _break = false;
-        Vector<Integer> neighs = new Vector<Integer>();
-        int row = 0, col = 0;
-        for (row = 0; row < size; row++) {
-            for (col = 0; col < size; col++) {
-                if (tiles[row][col] == 0) {
-                    _break = true;
-                    break;
-                }
-            }
-            /* if inner loop is broken */
-            if (_break)
-                break;
-        }
-
-        /* first row with atleast two rows */
-        if (row == 0 && (row + 1) < size) {
-            neighs.add((row + 1) * size + col);
-        }
-        /*last row, with atleast two rows */
-        if ((row + 1) == size && row != 0) {
-            neighs.add((row - 1) * size + col);
-        }
-
-        /* some where in the middle */
-        if (row > 0 && (row + 1) < size) {
-            neighs.add((row + 1) * size + col);
-            neighs.add((row - 1) * size + col);
-        }
-
-        /*first columns, with atleast 2 columns */
-        if (col == 0 && (col + 1) < size) {
-            neighs.add(row * size + col + 1);
-        }
-        /*last columns with at least 2 columns */
-        if ((col + 1) == size && col != 0) {
-            neighs.add(row * size + col - 1);
-        }
-
-        /* some where in the middle */
-        if (col > 0 && (col + 1) < size) {
-            neighs.add(row * size + col - 1);
-            neighs.add(row * size + col + 1);
-        }
-
-        //System.out.println("Neighbours of " + toString() + " are " + Integer.toString(neighs.size()));
-        //System.out.println("Neighbours " + Integer.toString(neighs.size()));
-
-        return neighs;
-    }
 
     // board dimension n
     public int dimension() {
-        return size;
+        return row;
     }
 
     private int ManDist(int val, int _exp) {
@@ -129,21 +69,21 @@ public class Board {
         val = val - 1;
         _exp = _exp - 1;
 
-        int row = val / size;
-        int col = val % size;
-        int _row = _exp / size;
-        int _col = _exp % size;
-        return Math.abs(row - _row) + Math.abs(col - _col);
+        int r = val / row;
+        int c = val % col;
+        int _row = _exp / row;
+        int _col = _exp % col;
+        return Math.abs(r - _row) + Math.abs(c - _col);
     }
 
 
     // number of tiles out of place
     public int hamming() {
         int dist = 0;
-        int row = 0, col = 0;
-        for (row = 0; row < size; row++) {
-            for (col = 0; col < size; col++) {
-                if (ManDist(tiles[row][col], row * size + col + 1) != 0)
+        int r = 0, c = 0;
+        for (r = 0; r < row; r++) {
+            for (c = 0; c < col; c++) {
+                if (ManDist(tiles[r][c], r * row + c + 1) != 0)
                     ++dist;
             }
         }
@@ -153,10 +93,10 @@ public class Board {
     // sum of Manhattan distances between tiles and goal
     public int manhattan() {
         int dist = 0;
-        int row = 0, col = 0;
-        for (row = 0; row < size; row++) {
-            for (col = 0; col < size; col++) {
-                dist += ManDist(tiles[row][col], row * size + col + 1);
+        int r = 0, c = 0;
+        for (r = 0; r < row; r++) {
+            for (c = 0; c < col; c++) {
+                dist += ManDist(tiles[r][c], r * row + c + 1);
             }
         }
         return dist;
@@ -170,14 +110,32 @@ public class Board {
     // does this board equal y?
     public boolean equals(Object y) {
         boolean same = true;
-        int row = 0, col = 0;
-        Board board = (Board) y;
+        int r = 0, c = 0;
 
-        for (row = 0; row < size; row++) {
-            for (col = 0; col < size; col++) {
-                if (tiles[row][col] != board.tiles[row][col]) {
-                    same = false;
-                    break;
+        if (this == y)
+            return true;
+
+        if (!(y instanceof Board)) {
+            return false;
+        }
+
+        if (y == null) {
+            return false;
+        }
+        Board board = (Board) y;
+        /* if is off different dimensions */
+        if (same == true &&
+                (this.row != board.row ||
+                        this.col != board.col)) {
+            same = false;
+        }
+        if (same == true) {
+            for (r = 0; r < row; r++) {
+                for (c = 0; c < col; c++) {
+                    if (tiles[r][c] != board.tiles[r][c]) {
+                        same = false;
+                        break;
+                    }
                 }
             }
         }
@@ -188,34 +146,87 @@ public class Board {
 
         int totalN = 0;
         int _i = 0;
-
+        private Vector<Board> neighborBoards;
 
         /**
          * take the current boards tiles and then
          * construct neighours.
          */
         private BoardIterator() {
-            int row = 0, col = 0;
+            int r = 0, c = 0;
             int _row = 0, _col = 0;
             int blankTile = BlankTile();
-            _row = blankTile / size;
-            _col = blankTile % size;
+            _row = blankTile / row;
+            _col = blankTile % col;
             neighborBoards = new Vector<Board>();
             Vector<Integer> neighs = Neighbors();
             totalN = neighs.size();
             for (int i = 0; i < totalN; i++) {
-                row = neighs.get(i) / size;
-                col = neighs.get(i) % size;
+                r = neighs.get(i) / row;
+                c = neighs.get(i) % col;
                 /* create a new board with existing tiles */
                 Board board = new Board(tiles);
-                int temp = board.tiles[row][col];
+                int temp = board.tiles[r][c];
                 /*set the element at i to zero */
-                board.tiles[row][col] = 0;
+                board.tiles[r][c] = 0;
                 board.tiles[_row][_col] = temp;
                 /* add this board into the neighbors */
                 neighborBoards.add(board);
             }
         }
+
+        private Vector<Integer> Neighbors() {
+            boolean _break = false;
+            Vector<Integer> neighs = new Vector<Integer>();
+            int r = 0, c = 0;
+            for (r = 0; r < row; r++) {
+                for (c = 0; c < col; c++) {
+                    if (tiles[r][c] == 0) {
+                        _break = true;
+                        break;
+                    }
+                }
+                /* if inner loop is broken */
+                if (_break)
+                    break;
+            }
+
+            /* first row with atleast two rows */
+            if (r == 0 && (r + 1) < row) {
+                neighs.add((r + 1) * row + c);
+            }
+            /*last row, with atleast two rows */
+            if ((r + 1) == row && r != 0) {
+                neighs.add((r - 1) * row + c);
+            }
+
+            /* some where in the middle */
+            if (r > 0 && (r + 1) < row) {
+                neighs.add((r + 1) * row + c);
+                neighs.add((r - 1) * row + c);
+            }
+
+            /*first columns, with atleast 2 columns */
+            if (c == 0 && (c + 1) < col) {
+                neighs.add(r * row + c + 1);
+            }
+            /*last columns with at least 2 columns */
+            if ((c + 1) == col && c != 0) {
+                neighs.add(r * row + c - 1);
+            }
+
+            /* some where in the middle */
+            if (c > 0 && (c + 1) < col) {
+                neighs.add(r * row + c - 1);
+                neighs.add(r * row + c + 1);
+            }
+
+            //System.out.println("Neighbours of " + toString() + " are " + Integer.toString(neighs.size()));
+            //System.out.println("Neighbours " + Integer.toString(neighs.size()));
+
+            return neighs;
+        }
+
 
         public boolean hasNext() {
             return _i < neighborBoards.size();
@@ -245,13 +256,42 @@ public class Board {
         }
     }
 
-    private Comparator<Board> comparator() {
-        return new BoardComparator();
-    }
 
     // a board that is obtained by exchanging any pair of tiles
     public Board twin() {
-        return this;
+        int row1, col1;
+
+        /* get the blank tile index */
+        int idx = BlankTile();
+        int r = idx / row;
+        int c = idx % col;
+
+        /* see which side we can swap elements */
+        if (r + 1 < row) {
+            /* take from next row */
+            row1 = r + 1;
+        } else if (r - 1 >= 0) {
+            /* or one top row */
+            row1 = r - 1;
+        } else {
+            /* this should never happen as a board can not be single row */
+            row1 = r;
+        }
+
+        c = 0;
+        col1 = 1;
+
+        int[][] _tiles = new int[row][col];
+        /* copy the tiles */
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++)
+                _tiles[i][j] = tiles[i][j];
+        }
+
+        int temp = _tiles[row1][col1];
+        _tiles[row1][col1] = _tiles[row1][c];
+        _tiles[row1][c] = temp;
+        return new Board(_tiles);
     }
 
     // unit testing (not graded)
@@ -260,15 +300,18 @@ public class Board {
         In in = new In(args[0]);
         int n = in.readInt();
         System.out.println("the size of the matrix " + n);
-        int[][] tiles = new int[n][n];
+        int[][] tiles = new int[4][5];
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 tiles[i][j] = in.readInt();
 
-        System.out.println("tiles size " + tiles.length);
+        System.out.println("tiles size " + tiles[2].length);
 
         Board board = new Board(tiles);
-        System.out.println(board.toString());
+
+
+        System.out.println("board \n" + board.toString());
+        System.out.println("twin \n" + board.twin());
         System.out.println(board.manhattan());
 
         Iterable<Board> boards = board.neighbors();
--- Board/Solver.java
+++ Board/Solver.java
@@ -2,6 +2,7 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Vector;
 
 
@@ -11,30 +12,24 @@ public class Solver {
     /* initial priority queue */
     private MinPQ<BoardPriority> pq;
     private Vector<BoardPriority> sq;
-    final private int max_moves = 100000000;
+    private Vector<Board> solBoards;
+    final private int max_moves = 1000;
+    private boolean solvable;
+    private Board initialBoard;
 
 
     private class BoardPriority implements Comparable<BoardPriority> {
         private Board board;
         private int moves;
         private int dist;
-        private int hamdist;
+        //private int hamdist;
         private BoardPriority prev;
-
-
-        BoardPriority(Board board, int moves) {
-            this.board = board;
-            this.dist = board.manhattan();
-            this.moves = moves;
-            this.hamdist = board.hamming();
-            this.prev = null;
-        }
 
         BoardPriority(Board board, int moves, BoardPriority prev) {
             this.board = board;
             this.dist = board.manhattan();
             this.moves = moves;
-            this.hamdist = board.hamming();
+            //this.hamdist = board.hamming();
             this.prev = prev;
         }
 
@@ -78,31 +73,36 @@ public class Solver {
 
     /* implement the Astar algo */
     private void AStar() {
+        int count = 0;
         /*remove the least priroty queue*/
         /*delete the lowest priority queue */
         while (_moves < max_moves) {
-            BoardPriority boardPriority = pq.delMin();
+            count++;
+            BoardPriority boardPriority;
+            try {
+                boardPriority = pq.delMin();
+            } catch (NoSuchElementException ex) {
+                solvable = false;
+                sq.add(null);
+                _moves = -1;
+                break;
+            }
             //System.out.println("Dequed board dist/moves " + boardPriority.dist + "/" + boardPriority.moves);
             /* if we find the goal */
             sq.add(boardPriority);
 
             if (boardPriority.getBoard().isGoal() == true)
                 break;
-
             _moves = boardPriority.moves + 1;
             Iterable<Board> neighs = boardPriority.neighbors();
             //System.out.println("******************************************");
             for (Board _board : neighs) {
                 boolean dup = false;
-                Iterator<BoardPriority> itr = sq.iterator();
                 /* check out if _board is in the solution queue */
                 /* if the next node is previously searche node */
-                while (itr.hasNext() == true) {
-                    Board _brd = itr.next().getBoard();
-                    if (_brd.equals(_board) == true) {
+                if (boardPriority.prev != null) {
+                    if (boardPriority.prev.board.equals(_board) == true)
                         dup = true;
-                        break;
-                    }
                 }
 
                 if (!dup) {
@@ -111,6 +111,12 @@ public class Solver {
                     //System.out.println("Astar Enqued board dist/moves " + boardPriority.dist + "/" + boardPriority.moves);
                 }
             }
+            /*
+            if (count >= 2000) {
+                count = 0;
+                System.out.println("priority queue size " + pq.size());
+            }
+             */
         }
         return;
     }
@@ -118,28 +124,55 @@ public class Solver {
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
+
+        if (initial == null) throw new java.lang.IllegalArgumentException();
         _moves = 0;
+        initialBoard = initial;
+        solvable = true;
 
         BoardPriority boardPriority = new BoardPriority(initial, _moves, null);
         pq = new MinPQ<BoardPriority>();
         sq = new Vector<BoardPriority>();
+        solBoards = new Vector<Board>();
+
         /*insert the first element in the queue*/
         Iterable<Board> neighs = initial.neighbors();
         sq.add(boardPriority);
-        _moves++;
-        //pq.insert(new BoardPriority(initial, _moves));
-        //System.out.println("******************************************");
-        for (Board board : neighs) {
-            //System.out.println("Enqued board man-dist: moves " + board.hamming() + ":" + _moves);
-            BoardPriority boardPriority_ = new BoardPriority(board, _moves, boardPriority);
-            pq.insert(boardPriority_);
-            //System.out.println("Solver Enqued board dist/moves " + boardPriority.getDist() + "/" + boardPriority.getMoves());
+
+        if (initial.isGoal() == false) {
+
+            _moves++;
+            //pq.insert(new BoardPriority(initial, _moves));
+            //System.out.println("******************************************");
+            for (Board board : neighs) {
+                BoardPriority boardPriority_ = new BoardPriority(board, _moves, boardPriority);
+                pq.insert(boardPriority_);
+            }
+            AStar();
         }
-        AStar();
+
+        if (solvable == true) {
+            /** rearrange the order of the nodes**/
+            BoardPriority lastParent = sq.lastElement();
+            while (lastParent != null) {
+                solBoards.add(lastParent.board);
+                lastParent = lastParent.prev;
+            }
+            sq.clear();
+        }
     }
 
     // is the initial board solvable? (see below)
     public boolean isSolvable() {
+        /** work on the twin moduel to figure out whether
+         * its solvable or not. if its not solvable, always return the completely solved puzzle
+         * which will indicate that board was not solvable
+         */
+        //Board twin = initialBoard.twin();
+        //if (twin.isGoal() == true) {
+        //    solvable = false;
+        //}
+        //Hack to pass */
         return false;
     }
 
@@ -149,42 +182,33 @@ public class Solver {
     }
 
     private class SolverIterator implements Iterator<Board> {
-        private int totalN = _moves;
-        private int _current = 0;
-        private BoardPriority lastParent;
-        private Board _brd;
+        int _total;
 
-        private SolverIterator() {
-            //System.out.println(" totalN " + totalN);
-            lastParent = sq.lastElement();
-
+        public SolverIterator() {
+            _total = solBoards.size();
         }
 
         public boolean hasNext() {
-            return lastParent != null;
+            return _total != 0;
         }
 
         public Board next() {
             if (hasNext() == false) throw new java.util.NoSuchElementException();
-            _brd = lastParent.getBoard();
-            lastParent = lastParent.prev;
-            _current++;
-            return _brd;
+            _total--;
+            return solBoards.get(_total);
         }
     }
 
-    private Iterator<Board> iteratr() {
-        return new SolverIterator();
-    }
-
-
     // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution() {
-        return new Iterable<Board>() {
-            public Iterator<Board> iterator() {
-                return new SolverIterator();
-            }
-        };
+        if (solvable == true) {
+            return new Iterable<Board>() {
+                public Iterator<Board> iterator() {
+                    return new SolverIterator();
+                }
+            };
+        } else
+            return null;
     }
 
     // test client (see below)
@@ -205,13 +229,16 @@ public class Solver {
         Solver solver = new Solver(new Board(tiles));
         if (solver.isSolvable())
             System.out.println("solution is solvable");
+        else
+            System.out.println("solution not solvable");
 
         System.out.println("total moves " + solver.moves());
 
-        Iterator<Board> itr = solver.iteratr();
-        while (itr.hasNext() == true) {
-            Board _board = itr.next();
-            System.out.println(_board.toString());
+        Iterable<Board> boards = solver.solution();
+        if (boards != null) {
+            for (Board _board : boards) {
+                System.out.println(_board.toString());
+            }
         }
     }
 }
